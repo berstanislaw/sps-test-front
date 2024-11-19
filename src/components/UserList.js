fa406e5f853c783a572bfc6baa8c3bee62de3fe5
Bernardo Garcia <brenado7@gmail.com>
2024-11-19 13:42:32 -0300

    removing unused var

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Modal from "react-modal";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import UserService from "../services/UserService";
 
 const modalStyles = {
